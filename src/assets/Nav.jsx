0d892a60794e2e3e713752229480f6e593cff699
julianavalenti <julianabmartinez2@icloud.com>
2023-05-24 17:07:05 -0400

    adds all gifs

--- src/assets/Nav.jsx
+++ src/assets/Nav.jsx
@@ -2,8 +2,48 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+  
+      <div>
+        {/* menu */}
+        <input
+          id="close-menu"
+          className="close-menu"
+          type="checkbox"
+          aria-label="close-menu"
+          role="button"
+        />
+        <label
+          className="close-menu-label"
+          htmlFor="close-menu"
+          title="close menu"
+        ></label>
+        <div className="barra">
+          <div className="menu-centro">
+            <div className="barra-conteiner centralizador">
+              
+              <nav className="direita">
+                <ul>
+                  <li>
+                    <a href="#inicio">Home</a>
+                  </li>
+                  <li>
+                    <a href="#sobre-mim">About Me</a>
+                  </li>
+                  <li>
+                    <a href="#projetos">Projects</a>
+                  </li>
+                  <li>
+                    <a href="#habilidades">Skills</a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  
+
 
 export default Nav
--- src/assets/pages/AboutMe.jsx
+++ src/assets/pages/AboutMe.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 const AboutMe = () => {
   return (
-    <div>AboutMe</div>
-  )
-}
+    <section id="sobre-mim">
+      <div className="centralizador">
+        <div className="ajustador-2">
+          <div className="avatar">
+            <img
+              className="desenho-bg"
+              src="/map.gif"
+              alt="map"
+            />
+            
+          </div>
+          <div className="sobre-mim">
+            <h2>About Me</h2>
+            <br />
+            <p>
+              blablabla add about me
+            </p>
+            <br />
+            <p>
+              add more about me
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
